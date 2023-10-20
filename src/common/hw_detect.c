b27fb4a52e5785bfc3828cb5c7490de62abcec13
Andrew Robbins <andrew@robbinsa.me>
2023-10-20 09:59:49 -0400

    lowkey forgot how to write c code

--- src/common/hw_detect.c
+++ src/common/hw_detect.c
@@ -2,22 +2,20 @@
 #include "hwloc.h"
 
 unsigned int get_l1_data_cache() {
-
-    hwloc_topology_t* topo;
-    hwloc_topology_init(topo);
-    hwloc_topology_load(*topo);
-    hwloc_obj_t myL1 = hwloc_get_obj_by_type(*topo, HWLOC_OBJ_L1CACHE, 0);
-    free(topo);
+    hwloc_topology_t topo;
+    hwloc_topology_init(&topo);
+    hwloc_topology_load(topo);
+    hwloc_obj_t myL1 = hwloc_get_obj_by_type(topo, HWLOC_OBJ_L1CACHE, 0);
+    hwloc_topology_destroy(topo);
     return myL1->attr->cache.size;
 }
 
 unsigned int get_l2_data_cache()
 {
-
-    hwloc_topology_t *topo;
-    hwloc_topology_init(topo);
-    hwloc_topology_load(*topo);
-    hwloc_obj_t myL2 = hwloc_get_obj_by_type(*topo, HWLOC_OBJ_L2CACHE, 0);
-    free(topo);
+    hwloc_topology_t topo;
+    hwloc_topology_init(&topo);
+    hwloc_topology_load(topo);
+    hwloc_obj_t myL2 = hwloc_get_obj_by_type(topo, HWLOC_OBJ_L2CACHE, 0);
+    hwloc_topology_destroy(topo);
     return myL2->attr->cache.size;
 }
