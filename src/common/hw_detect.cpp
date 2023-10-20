eef275f740987957a4f7d7e1f01819e87d3fcdc6
Andrew Robbins <andrew@robbinsa.me>
2023-10-20 09:17:39 -0400

    fix leaks

--- src/common/hw_detect.cpp
+++ src/common/hw_detect.cpp
@@ -7,6 +7,7 @@ unsigned int get_l1_data_cache() {
     hwloc_topology_init(topo);
     hwloc_topology_load(*topo);
     hwloc_obj_t myL1 = hwloc_get_obj_by_type(*topo, HWLOC_OBJ_L1CACHE, 0);
+    free(topo);
     return myL1->attr->cache.size;
 }
 
@@ -17,5 +18,6 @@ unsigned int get_l2_data_cache()
     hwloc_topology_init(topo);
     hwloc_topology_load(*topo);
     hwloc_obj_t myL2 = hwloc_get_obj_by_type(*topo, HWLOC_OBJ_L2CACHE, 0);
+    free(topo);
     return myL2->attr->cache.size;
 }
