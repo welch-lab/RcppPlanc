df28a3851cec083d8052bd710c77abc14147051a
Andrew Robbins <andrew@robbinsa.me>
2023-10-20 10:55:56 -0400

    fix use after free

--- src/common/hw_detect.c
+++ src/common/hw_detect.c
@@ -6,8 +6,9 @@ unsigned int get_l1_data_cache() {
     hwloc_topology_init(&topo);
     hwloc_topology_load(topo);
     hwloc_obj_t myL1 = hwloc_get_obj_by_type(topo, HWLOC_OBJ_L1CACHE, 0);
+    hwloc_uint64_t myL1size = myL1->attr->cache.size;
     hwloc_topology_destroy(topo);
-    return myL1->attr->cache.size;
+    return myL1size;
 }
 
 unsigned int get_l2_data_cache()
@@ -16,6 +17,7 @@ unsigned int get_l2_data_cache()
     hwloc_topology_init(&topo);
     hwloc_topology_load(topo);
     hwloc_obj_t myL2 = hwloc_get_obj_by_type(topo, HWLOC_OBJ_L2CACHE, 0);
+    hwloc_uint64_t myL2size = myL2->attr->cache.size;
     hwloc_topology_destroy(topo);
-    return myL2->attr->cache.size;
+    return myL2size;
 }
