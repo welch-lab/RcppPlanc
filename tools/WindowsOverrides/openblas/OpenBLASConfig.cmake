8b6a2f748702682eb647341a98d74781c04e475f
Andrew Robbins <andrew@robbinsa.me>
2023-11-14 14:51:16 -0500

    include fixed config overrides

---
+++ tools/WindowsOverrides/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.24")
+SET(OpenBLAS_INCLUDE_DIRS /usr/lib/mxe/usr/x86_64-w64-mingw32.static.posix/include/openblas)
+SET(OpenBLAS_LIBRARIES -L/usr/lib/mxe/usr/x86_64-w64-mingw32.static.posix/lib/ -lopenblas -lpthread -fopenmp -lgfortran -lquadmath)
