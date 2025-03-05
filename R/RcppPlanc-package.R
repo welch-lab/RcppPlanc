ee93172a42912e5c5ea0e58d607ec0a99c8371d4
Andrew Robbins <andrew@robbinsa.me>
2025-03-05 13:08:20 -0500

    work around R CMD CHECK being stupid

--- R/RcppPlanc-package.R
+++ R/RcppPlanc-package.R
@@ -1,5 +1,6 @@
 ## usethis namespace: start
-#' @rawNamespace useDynLib(libnmflib, .registration=TRUE); useDynLib(RcppPlanc, .registration=TRUE)
+#' @rawNamespace useDynLib(libnmflib, .registration=TRUE)
+#' useDynLib(RcppPlanc, .registration=TRUE)
 #' @importFrom Rcpp sourceCpp
 #' @exportPattern "^[[:alpha:]]+"
 ## usethis namespace: end
