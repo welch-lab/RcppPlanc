fd26d38e04fbfbea09ef34d07cd56fc6f5581de0
Andrew Robbins <andrew@robbinsa.me>
2025-03-05 12:55:52 -0500

    fix packageload

--- R/RcppPlanc-package.R
+++ R/RcppPlanc-package.R
@@ -1,5 +1,5 @@
 ## usethis namespace: start
-#' @useDynLib RcppPlanc, .registration=TRUE
+#' @rawNamespace useDynLib(libnmflib, .registration=TRUE); useDynLib(RcppPlanc, .registration=TRUE)
 #' @importFrom Rcpp sourceCpp
 #' @exportPattern "^[[:alpha:]]+"
 ## usethis namespace: end
