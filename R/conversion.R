52ca8329bb6569eb11509919f0415d49bc0ef573
Yichen Wang <wayichen@umich.edu>
2025-03-11 16:36:42 -0400

    Fix R-side peripheral check errors

--- R/conversion.R
+++ R/conversion.R
@@ -1,11 +1,11 @@
-rcpp_mat_to_h5mat <- function(x, filename, dataPath, overwrite = TRUE, ...) {
+.rcpp_mat_to_h5mat <- function(x, filename, dataPath, overwrite = TRUE, ...) {
   res <- c(filename, dataPath)
   # Set the dimensions of the dataset
   hdf5r.Extra::h5Write(x = x, file = filename, name = dataPath, overwrite = overwrite, transpose = FALSE, ...)
   return(res)
 }
 
-rcpp_spmat_to_h5mat <- function(x, filename, dataPath, overwrite = TRUE, ...)
+.rcpp_spmat_to_h5mat <- function(x, filename, dataPath, overwrite = TRUE, ...)
 {
   res <- c(filename, dataPath)
   x <- as.matrix(x)
@@ -13,7 +13,7 @@ rcpp_spmat_to_h5mat <- function(x, filename, dataPath, overwrite = TRUE, ...)
   return(res)
 }
 
-rcpp_spmat_to_h5spmat <- function(x, filename, dataPath, overwrite = TRUE) {
+.rcpp_spmat_to_h5spmat <- function(x, filename, dataPath, overwrite = TRUE) {
   res <- c(filename, paste0(dataPath, "/data"), paste0(dataPath, "/indices"), paste0(dataPath, "/indptr"))
   hdf5r.Extra::h5Write(x = x, file = filename, name = dataPath, overwrite = overwrite, transpose = FALSE)
   return(res)
